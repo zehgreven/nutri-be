27197f7cc76a76d70aee391b89734007423dd7e2
José Lazaretti <eduardo1793@gmail.com>
2022-08-17 20:24:07 -0300

    Adding put

--- src/controllers/functionality.v1.ts
+++ src/controllers/functionality.v1.ts
@@ -1,4 +1,4 @@
-import { ClassMiddleware, Controller, Get, Post } from '@overnightjs/core';
+import { ClassMiddleware, Controller, Get, Post, Put } from '@overnightjs/core';
 import logger from '@src/logger';
 import { authMiddleware } from '@src/middlewares/auth';
 import { rateLimiter } from '@src/middlewares/rate-limit';
@@ -29,6 +29,36 @@ export class FunctionalityControllerV1 extends BaseController {
 
       const result = await this.repository.create(req.body);
       res.status(201).send(result);
+    } catch (error) {
+      this.sendCreateUpdateErrorResponse(res, error);
+    }
+  }
+
+  @Put(':id')
+  public async update(req: Request, res: Response): Promise<void> {
+    try {
+      if (!req.context?.userId) {
+        this.sendErrorResponse(res, {
+          code: StatusCodes.INTERNAL_SERVER_ERROR,
+          message: 'Something went wrong',
+        });
+        logger.error('Missing userId');
+        return;
+      }
+
+      const requestParamId = req.params?.id;
+
+      if (!requestParamId) {
+        this.sendErrorResponse(res, {
+          code: StatusCodes.INTERNAL_SERVER_ERROR,
+          message: 'Something went wrong',
+        });
+        logger.error('Missing parameter id');
+        return;
+      }
+
+      await this.repository.update(requestParamId, req.body);
+      res.status(StatusCodes.NO_CONTENT).send();
     } catch (error) {
       this.sendCreateUpdateErrorResponse(res, error);
     }
