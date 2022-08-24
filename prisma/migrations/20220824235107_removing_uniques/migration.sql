f53108b201121f86fe0538d00ff2ee8997e06a02
José Lazaretti <eduardo1793@gmail.com>
2022-08-24 20:52:21 -0300

    Removing uniques

---
+++ prisma/migrations/20220824235107_removing_uniques/migration.sql
@@ -0,0 +1,14 @@
+-- DropIndex
+DROP INDEX "Functionality_functionalityTypeId_key";
+
+-- DropIndex
+DROP INDEX "ProfilePermission_functionalityId_key";
+
+-- DropIndex
+DROP INDEX "ProfilePermission_profileId_key";
+
+-- DropIndex
+DROP INDEX "UserPermission_functionalityId_key";
+
+-- DropIndex
+DROP INDEX "UserPermission_userId_key";
