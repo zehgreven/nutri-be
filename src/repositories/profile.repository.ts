478e7aacf4cbec978286a278153d41e170900fb6
José Lazaretti <eduardo1793@gmail.com>
2022-08-27 18:30:15 -0300

    Adding findProfilesByUser

--- src/repositories/profile.repository.ts
+++ src/repositories/profile.repository.ts
@@ -11,4 +11,42 @@ export class ProfileRepository
   constructor() {
     super(prismaClient.profile);
   }
+
+  public async findProfilesByUser(
+    userId: string,
+    options: Partial<Profile>,
+    paging: Paging,
+  ): Promise<Paginated<Profile>> {
+    try {
+      const { limit, page } = paging;
+      const where = this.buildWhere(options);
+
+      const result = await prismaClient.profile.findMany({
+        skip: limit * page,
+        take: limit,
+
+        orderBy: {
+          created_at: 'desc',
+        },
+      });
+
+      const count = await prismaClient.profile.count({ where });
+
+      const previousPage = page > 0 ? page : undefined;
+      const totalPages = Math.max(Math.ceil(count / limit), 1);
+      const nextPage = page + 2 > totalPages ? undefined : page + 2;
+
+      return {
+        result,
+        page: page + 1,
+        previousPage,
+        nextPage,
+        totalPages,
+        limit,
+        count,
+      };
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
 }
