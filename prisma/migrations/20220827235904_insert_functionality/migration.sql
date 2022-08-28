41ade3c10d8cd214999c82af72324b9ed41220a9
José Lazaretti <eduardo1793@gmail.com>
2022-08-28 00:10:04 -0300

    Fixing name vs description

--- prisma/migrations/20220827235904_insert_functionality/migration.sql
+++ prisma/migrations/20220827235904_insert_functionality/migration.sql
@@ -1,4 +1,4 @@
-INSERT INTO public."Functionality" (id, active, created_at, updated_at, deleted_at, description, name, path, "functionalityTypeId", "masterId", "icon") VALUES 
+INSERT INTO public."Functionality" (id, active, created_at, updated_at, deleted_at, name, description, path, "functionalityTypeId", "masterId", "icon") VALUES 
 
 ('8c29a940-463c-4670-914a-ef57cfb8cb3e', true, '2022-08-27 00:00:00.00', NULL, NULL, 'Início', 'Página inicial', '', '52a19d20-9904-4a2d-b7c5-ff3d25b80e41', NULL, 'home'),
 ('4356f7c3-a892-4183-8c8a-948a35449576', true, '2022-08-27 00:00:00.00', NULL, NULL, 'Controle de Acesso', 'Menu de controle de acesso', null, '52a19d20-9904-4a2d-b7c5-ff3d25b80e41', NULL, 'Settings'),
