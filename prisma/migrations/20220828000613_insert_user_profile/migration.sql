070c4acfd8eba32f3817f0a503332304349aab24
José Lazaretti <eduardo1793@gmail.com>
2022-08-27 21:07:59 -0300

    Migration to insert default data

---
+++ prisma/migrations/20220828000613_insert_user_profile/migration.sql
@@ -0,0 +1,2 @@
+INSERT INTO public."UserProfile" (id, active, created_at, updated_at, deleted_at, "profileId", "userId") VALUES 
+('d87f0a9f-ad0f-42f4-b901-71b633746f9a', true, '2022-08-27 00:00:00.000', NULL, NULL, 'a81b2f88-fe8f-492c-aca5-7b09983c007d', '13782202-684d-4ba9-9cad-487b9e4a6a8a');
